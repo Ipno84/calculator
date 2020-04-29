1ccb03b3ee5ff4695899bb4081437d6ade680835
Marco Abate <m.abate@logispin.com>
2020-04-29 18:42:26 +0200

    Updates

---
+++ precache-manifest.109ab6f7eaeae4e26b8cef8cfbdd8289.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76abfd43bb5d4a20b0719907ac3e8d73",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "6b0816ce4cd769a0dd9c",
+    "url": "/calculator/static/css/main.26f54712.chunk.css"
+  },
+  {
+    "revision": "57f9fe3b73d31d6ad1e5",
+    "url": "/calculator/static/js/2.006d60d2.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/calculator/static/js/2.006d60d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0816ce4cd769a0dd9c",
+    "url": "/calculator/static/js/main.c8741c2e.chunk.js"
+  },
+  {
+    "revision": "76b2aec7ab6e360446a5",
+    "url": "/calculator/static/js/runtime-main.a1af8741.js"
+  },
+  {
+    "revision": "f62f26a8af03bb53a0a83dd23a2fbbd7",
+    "url": "/calculator/static/media/ProductSans-Black.f62f26a8.woff"
+  },
+  {
+    "revision": "ef0c4740e420c336c3df028d8b41a105",
+    "url": "/calculator/static/media/ProductSans-BlackItalic.ef0c4740.woff"
+  },
+  {
+    "revision": "56831c947d8b6e5fdd01e9ebc4576ce2",
+    "url": "/calculator/static/media/ProductSans-Bold.56831c94.woff"
+  },
+  {
+    "revision": "bb2ed9e7721d98afd8652c6a60a31b51",
+    "url": "/calculator/static/media/ProductSans-BoldItalic.bb2ed9e7.woff"
+  },
+  {
+    "revision": "124ecceef2b99a38e7037a6cef9db5a4",
+    "url": "/calculator/static/media/ProductSans-Italic.124eccee.woff"
+  },
+  {
+    "revision": "25097357bda74912851eb5256a4699ff",
+    "url": "/calculator/static/media/ProductSans-Light.25097357.woff"
+  },
+  {
+    "revision": "7524fd2e8df80159218aaace1720b991",
+    "url": "/calculator/static/media/ProductSans-LightItalic.7524fd2e.woff"
+  },
+  {
+    "revision": "ec26aae8ba4c449f686b73576f5baf2b",
+    "url": "/calculator/static/media/ProductSans-Medium.ec26aae8.woff"
+  },
+  {
+    "revision": "255754fa1966557f15e24cca12b4aafb",
+    "url": "/calculator/static/media/ProductSans-MediumItalic.255754fa.woff"
+  },
+  {
+    "revision": "f8c351bf8263f7beefb5c4c444951389",
+    "url": "/calculator/static/media/ProductSans-Regular.f8c351bf.woff"
+  },
+  {
+    "revision": "2d7cc69ae28dee37020ddbd4a01d15d3",
+    "url": "/calculator/static/media/ProductSans-Thin.2d7cc69a.woff"
+  },
+  {
+    "revision": "833fe2e55d597e46178eba86e49fb31a",
+    "url": "/calculator/static/media/ProductSans-ThinItalic.833fe2e5.woff"
+  },
+  {
+    "revision": "a00f0ef02f5ae9e72bd6898462cbfadd",
+    "url": "/calculator/static/media/backspace.a00f0ef0.svg"
+  }
+]);
